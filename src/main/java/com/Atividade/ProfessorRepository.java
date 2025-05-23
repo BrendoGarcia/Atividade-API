dbc9fb090fb3c9d27c24e1493a055bcd9b97abb6
Brendo Garcia da Silva <86546389+BrendoGarcia@users.noreply.github.com>
2025-05-23 18:51:26 -0300

    Update ProfessorRepository.java

--- src/main/java/com/Atividade/ProfessorRepository.java
+++ src/main/java/com/Atividade/ProfessorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ProfessorRepository extends CrudRepository<Professor, String>{
 	
-	Professor findBycpfcolaborador(String cpfcolaborador);
+	Professor findBycpfprofessor(String cpfprofessor);
 
 }
